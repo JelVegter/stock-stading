17c72129376c98db2669a7a045087389d048e8b4
Jelle Vegter <jelle.vegter@nederlandseloterij.nl>
2023-02-05 10:54:09 +0100

    init

---
+++ stock_trading/main.py
@@ -0,0 +1,22 @@
+# clientportal.gw/bin/run.sh clientportal.gw/root/conf.yaml
+from common.interactive_brokers import InteractiveBrokersApi
+
+
+def main():
+    api = InteractiveBrokersApi()
+    status_code = api.fetch_status_code("user")
+    assert status_code == 200
+    symbol = "AAPL"
+
+    # stock_contracts = api.fetch_stock_contracts(symbol)
+    # pprint(stock_contracts)
+
+    AAPL_con_id = 265598
+    MSFT_con_id = 272093
+    con_ids = [AAPL_con_id, MSFT_con_id]
+    stock_histories = api.fetch_stock_price_history(conid=con_ids)
+    print(stock_histories)
+
+
+if __name__ == "__main__":
+    main()
---
+++ stock_trading/portfolio/ib_account.py
@@ -0,0 +1,9 @@
+from common.interactive_brokers import ib_api
+
+
+def main():
+    ib_api.fetch_ib_account()
+
+
+if __name__ == "__main__":
+    main()
